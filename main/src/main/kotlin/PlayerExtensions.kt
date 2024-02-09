cd31f6d3d6b804a498e8fadcb500985a37ccd436
Jordon <98439832+jordoncodes@users.noreply.github.com>
2024-02-10 03:16:18 +1000

    rename extension function Player.setNickname(String) to Player.setNick(String)

--- main/src/main/kotlin/PlayerExtensions.kt
+++ main/src/main/kotlin/PlayerExtensions.kt
@@ -11,7 +11,7 @@ class PlayerExtensions {
             return Nicknamer.getDisguiser().getNick(this);
         }
 
-        fun Player.setNickname(nick: String) {
+        fun Player.setNick(nick: String) {
             Nicknamer.getDisguiser().setNick(this, nick);
         }
 
